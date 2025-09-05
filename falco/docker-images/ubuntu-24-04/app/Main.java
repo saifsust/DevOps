38c12a010fd42a144edc8418036d02d69621b8f5
saiful-islam <saiful.sust.cse@gmail.com>
2025-09-05 15:10:55 +0600

    [falco] docker image for falco practices

--- falco/docker-images/ubuntu-24-04/app/Main.java
+++ falco/docker-images/ubuntu-24-04/app/Main.java
@@ -2,7 +2,7 @@ public class Main{
     public static void main(String[] args){
         System.out.println("Hacker Attcher to read file");
         for(int i = 0; i<args.length; i++){
-            System.out.println(args);
+            System.out.println(args[i]);
         }
     }
 }
