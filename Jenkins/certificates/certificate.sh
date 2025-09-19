98e41aff95fd9a10251b68e79686c8f0fb529acf
saiful-islam <saiful.sust.cse@gmail.com>
2025-09-19 21:07:44 +0600

    [Jenkins] docker TLS certification

---
+++ Jenkins/certificates/certificate.sh
@@ -0,0 +1,17 @@
+openssl genrsa -aes256 -passout pass:bjit1234 -out ca.key 2048
+
+openssl req -new -x509 -days 3650 -passin pass:bjit1234 -key ca.key -subj="/CN=docker-ca/O=controlplane" -extensions v3_ca -config openssl.cnf -out ca.pem
+
+openssl genrsa -out docker-server-key.pem 2048
+
+openssl req -new -key docker-server-key.pem -subj="/CN=docker-server/O=controlplane" -extensions v3_ca -config openssl.cnf -out docker-server.csr
+
+openssl x509 -days 3650 -req -in docker-server.csr -passin pass:bjit1234 -CA ca.pem -CAkey ca-key.pem -CAcreateserial -extensions v3_ca -extfile openssl.cnf -out docker-server.pem
+
+openssl genrsa -out key.pem 2048
+
+openssl req -new -subj="/CN=client/O=controlplane" -key key.pem -extensions v3_ca -config openssl.cnf -out client.csr
+
+openssl x509 -days 3650 -req -in client.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -passin pass:bjit1234 -extensions v3_ca -extfile openssl.cnf -out cert.pem
+
+rm client.csr docker-server.csr
