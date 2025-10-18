6ba4621ea4f68aa24a1325e62b975ae59ce137a3
saiful-islam <saiful.sust.cse@gmail.com>
2025-10-18 11:54:27 +0600

    [Jenkins] shared libraries basic

--- Jenkins/shared-library/lib/vars/podCD.groovy
+++ Jenkins/shared-library/lib/vars/podCD.groovy
@@ -11,7 +11,7 @@ def call(def podName, def namespace, def images){
                 steps {
                     script{
                         println podYaml
-                        sh  'echo $podYaml | kubectl apply -f -'
+                        sh  'echo ${podYaml}'
                     }
                 }
                 post {
