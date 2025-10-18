6fb83088d5f3b6611878edd5c2c602c68ef1957c
saiful-islam <saiful.sust.cse@gmail.com>
2025-10-18 13:03:30 +0600

    [Jenkins] shared libraries basic

--- Jenkins/shared-library/lib/vars/podCD.groovy
+++ Jenkins/shared-library/lib/vars/podCD.groovy
@@ -5,7 +5,7 @@ def call(def podName, def namespace, def images) {
 
     def podUtil = (new PodUtils())
     def podYaml = (podUtil).getDeployYaml(podName, namespace, images)
-    (podUtil).writeYaml(podYaml, String.format("%s/%s", env.JENKINS_HOME, env.WORKSPACE, env.JOB_NAME))
+    (podUtil).writeYaml(podYaml, String.format("%s/%s/%s", env.JENKINS_HOME, env.WORKSPACE, env.JOB_NAME))
     pipeline {
         agent any
         environment {
