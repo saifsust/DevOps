d73d0a18cb5d460d05221c65014f134b97e0e131
saiful-islam <saiful.sust.cse@gmail.com>
2025-10-18 23:35:04 +0600

    [jenkins] CI modified

--- Jenkins/shared-library/lib/vars/podCD.groovy
+++ Jenkins/shared-library/lib/vars/podCD.groovy
@@ -1,22 +1,23 @@
 import org.aktota.utils.PodUtils
 
-def call() {
+def call(def appName) {
     def podUtil = (new PodUtils())
+    def appUtil = (new AppUtils())
     pipeline {
         agent any
         environment {
             GIT_CREDENTIAL_ID = 'gitRootAccess'
+            GIT_REPOSITORY = "${appUtil.applications()[appName]}"
             DOCKER_IMAGE_VERSION = ''
         }
         parameters {
-            choice(name: 'APP_NAME', description: 'Choose app for deploying', choices: getAppNames())
             string(name: 'GIT_BRANCH', defaultValue: 'master', description: 'Git Branch to build docker image', trim: true)
             choice(name: 'DEPLOY_ENV', choices: ['cks', 'graphql-system', 'cka', 'test'])
         }
         stages {
             stage('Preset and Git Checkout') {
                 steps {
-                    git url: applications()[APP_NAME], branch: "${params.GIT_BRANCH.trim()}", credentialsId: GIT_CREDENTIAL_ID
+                    git url: GIT_REPOSITORY, branch: "${params.GIT_BRANCH.trim()}", credentialsId: GIT_CREDENTIAL_ID
                 }
                 post {
                     success {
@@ -105,16 +106,5 @@ def call() {
 
         }
     }
-}
-
-def getAppNames() {
-    return applications().keySet().toList()
-}
-
-def applications() {
-    return [
-            'mock-server': 'https://github.com/saifsust/mock-server.git',
-            'workspace'  : 'https://github.com/saifsust/workspace.git'
-    ]
 }
 
