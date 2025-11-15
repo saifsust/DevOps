f9fea1f2b9848c1829bb8d952a8244f5f64e07c8
saiful-islam <saiful.sust.cse@gmail.com>
2025-11-15 15:10:10 +0600

    [Istio][Product APP] debugging enabled.

--- istio/mysql/mysqldb-init.sql
+++ istio/mysql/mysqldb-init.sql
@@ -9,5 +9,5 @@ CREATE TABLE `ratings` (
   `Rating` INT,
   PRIMARY KEY (`ReviewID`)
 );
-INSERT INTO ratings (ReviewID, Rating) VALUES (1, 10);
-INSERT INTO ratings (ReviewID, Rating) VALUES (2, 11);
+INSERT INTO ratings (ReviewID, Rating) VALUES (1, 1);
+INSERT INTO ratings (ReviewID, Rating) VALUES (2, 10);
