8eb7ed5ffc04e84253d1ff8e8ab8afd14fc0c97b
saiful-islam <saiful.sust.cse@gmail.com>
2025-12-01 15:12:14 +0600

    [OpenSSL][webhook] certification

--- access-control/webhook/.pipeline/manifest/istio/certs/generate.sh
+++ access-control/webhook/.pipeline/manifest/istio/certs/generate.sh
@@ -6,8 +6,8 @@ openssl req -new -x509 -days 3560 -key ca.key -subj="/CN=kubernetes/O=webhook" -
 
 # Client certificate adn key generation
 openssl genrsa -out client.key 2048
-openssl req -new -key client.key -subj="/CN=192.168.0.30/O=webhook" -out client.csr
-openssl x509 -req -in client.csr -days 3560 -passin pass:bjit1234 -CA ca.crt -CAkey ca.key -out client.crt
+openssl req -new -key client.key -subj="/CN=192.168.0.30/O=webhook" -config csr.conf  -out client.csr
+openssl x509 -req -in client.csr -days 3560 -passin pass:bjit1234 -CA ca.crt -CAkey ca.key -extensions v3_req -extfile csr.conf -out client.crt
 
 openssl verify -CAfile ca.crt client.crt
 
