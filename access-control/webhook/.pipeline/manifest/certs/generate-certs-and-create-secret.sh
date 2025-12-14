54621666f09fd138097c5ce8c183e50de7e63413
saiful-islam <saiful.sust.cse@gmail.com>
2025-12-14 16:34:50 +0600

    [Webhook] organized the test and bash resource

--- access-control/webhook/.pipeline/manifest/certs/generate-certs-and-create-secret.sh
+++ access-control/webhook/.pipeline/manifest/certs/generate-certs-and-create-secret.sh
@@ -6,12 +6,11 @@ openssl req -new -x509 -days 3560 -key ca.key -subj="/CN=kubernetes/O=webhook" -
 
 # Client certificate adn key generation
 openssl genrsa -out client.key 2048
-openssl req -new -key client.key -subj="/CN=192.168.0.30/O=webhook" -config csr.conf  -out client.csr
+openssl req -new -key client.key -subj="/CN=$(cat host)/O=webhook" -config csr.conf  -out client.csr
 openssl x509 -req -in client.csr -days 3560 -passin pass:bjit1234 -CA ca.crt -CAkey ca.key -extensions v3_req -extfile csr.conf -out client.crt
 
 openssl verify -CAfile ca.crt client.crt
 
 rm *.csr
 
-kubectl -n istio-system delete secret webhook-credentials
 kubectl -n istio-system create secret tls webhook-credentials --cert=client.crt --key=client.key
