7121e10257b55074d822e95b619bab07713d768f
saiful-islam <saiful.sust.cse@gmail.com>
2025-12-20 23:42:50 +0600

    [BOM] query and generate document

--- security/sbom/generate-sbom.sh
+++ security/sbom/generate-sbom.sh
@@ -5,3 +5,7 @@ bom document outline spdx.json
 bom document query --format=json --fields=version  spdx.json 'name:openssl'
 
 bom document query --format=json --fields=version,name,supplier,url,originator  spdx.json 'name:openssl'
+
+bom generate --image=saifsust/non-privileged-nginx:stable --format=json --output=nginx.json
+
+bom document query --fields=name,version,url,license --format=json nginx.json 'name:nginx'
