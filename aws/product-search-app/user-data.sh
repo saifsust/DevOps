bb0ae5029747ba289980b66addb19f908646b50a
saiful-islam <saiful.sust.cse@gmail.com>
2026-02-14 15:04:59 +0600

    [AWS] product search app deploy

--- aws/product-search-app/user-data.sh
+++ aws/product-search-app/user-data.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 # Proxy Server Installation
 sudo apt-get install nginx -y
+
+## Docker Installation
+sudo apt update
+sudo apt install ca-certificates curl -y
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io -y
+sudo docker run -d -p 9080:9080 saifsust/productpage:1.0.98
+
+# Proxy Setup
 sudo rm -f /etc/nginx/sites-available/proxy-conf
 config="/etc/nginx/sites-available/proxy-conf"
 sudo cat <<EOF > $config
@@ -29,22 +50,3 @@ EOF
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /etc/nginx/sites-available/proxy-conf /etc/nginx/sites-enabled/default
 sudo systemctl restart nginx
-
-## Docker Installation
-sudo apt update
-sudo apt install ca-certificates curl -y
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
-Types: deb
-URIs: https://download.docker.com/linux/ubuntu
-Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
-Components: stable
-Signed-By: /etc/apt/keyrings/docker.asc
-EOF
-
-sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io -y
-sudo docker run -d -p 9080:9080 saifsust/productpage:1.0.98
